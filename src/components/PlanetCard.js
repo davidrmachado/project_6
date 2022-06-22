269cca41f5c76ef53a4f0f055a8c3c413e8bf71a
David-Neto <davidrmachado@gmail.com>
2022-06-22 19:32:43 -0300

    Corrigido erro de ESLint no arquivo CSS

--- src/components/PlanetCard.js
+++ src/components/PlanetCard.js
@@ -6,9 +6,9 @@ class PlanetCard extends React.Component {
     const { planetName } = this.props;
     const { planetImage } = this.props;
     return (
-      <div data-testid="planet-card">
+      <div data-testid="planet-card" className="planeta">
         <img src={ planetImage } alt={ `Planeta ${planetName}` } />
-        <p data-testid="planet-name">{planetName}</p>
+        <p className="titulo" data-testid="planet-name">{planetName}</p>
       </div>
     );
   }
--- src/components/SolarSystem.js
+++ src/components/SolarSystem.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import Title from './Title';
 import planets from '../data/planets';
 import PlanetCard from './PlanetCard';
 
@@ -7,7 +6,6 @@ class SolarSystem extends React.Component {
   render() {
     return (
       <div data-testid="solar-system">
-        <Title headline="Planetas" />
         <span>
           {planets.map((planet) => (
             <PlanetCard
